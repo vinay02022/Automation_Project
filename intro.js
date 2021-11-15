2b2edc659bc509cc715e366891c88f88fd7535a3
vinay <vinay01431998@gmail.com>
2021-11-15 12:23:06 +0530

    VisibleTrue

--- intro.js
+++ intro.js
@@ -1,14 +1,23 @@
 const puppeteer=require("puppeteer");
-let pages;
+let page;
 let browserOpenKaPromise=puppeteer.launch({headless:false});
 browserOpenKaPromise.then(function(browser){
     //console.log("your Nrowser Opened");
     const pagesArrpromise=browser.pages();
     return pagesArrpromise;
 }).then(function(browserPages){
-    cPage=browserPages[0];
-    let gotoPromise=cPage.goto("https://www.google.com/")
+    page=browserPages[0];
+    let gotoPromise=page.goto("https://www.google.com/")
     return gotoPromise;
-}).then(function(){
-    console.log("Reached On Google.com");
+}).then(()=>{
+    let elemWaitPromise=page.waitForSelector("input[type='text']",{visible:true})
+    return elemWaitPromise;
 })
+.then(function(){
+    //console.log("Reached On Google.com");
+    let keyWillSendPromise=page.type("input[type='text']","pepcoding.com")
+    return keyWillSendPromise;
+}).then(function(){
+    let enterWillBePressed=page.keyboard.press("Enter");
+    return enterWillBePressed;
+}).catch((err)=>console.log("Error Bro"+"  "+err));
